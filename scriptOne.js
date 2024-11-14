809dbd014a3ac6e036c679ea8eeb72e039cd716c
scarlettdaprile <skd53@njit.edu>
2024-11-14 11:05:24 -0500

    update

--- scriptOne.js
+++ scriptOne.js
@@ -47,9 +47,67 @@ if (startValue = 0) {
     console.log(house3)
 }
 if (startValue < 0) {
-    house3= "Number is negative"
+    house3= "Number is negative";
     console.log(house3)
 }
 //Q6
+let a= "15"
+let b= "13"
+console.log(a == b)
+console.log(a === b)
+console.log(a != b)
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+//Q7
+let firstName = "Scarlett"
+console.log(firstName)
+let lastName = "Daprile"
+console.log(lastName)
+let myAge = 18
+console.log(myAge)
+console.log("Hello,", firstName, lastName, "you are", myAge, "years old")
+//Q8
+let num=100
+if (num>0 && num<100) {
+console.log ("The number is within range")
+}
+//Q9
+let hasCar = true
+
+let hasLicense = true
+
+let hasInsurance = true
+
+
+if (hasCar && hasLicense && hasInsurance) {
+   console.log("You can drive legally.");
+}
+else {
+   console.log("Check your driving eligibility.");
+}
+//Q10
+let num2=21
+if (num2)
+
+//Q11
+let house4;
+let number2="85"
+
+if (number2 > 90) {
+    house = "Grade A";
+    console.log(house);
+}
+
+else if(number2>80){
+    house= "Grade B";
+    console.log(house);
+}
+
+else if(number2>70){
+    house="Grade C";
+    console.log(house);
+}
 
 
