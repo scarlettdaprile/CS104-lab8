c6f6e9d732eaf3470aadee98536c07fc4db35af3
scarlettdaprile <skd53@njit.edu>
2024-11-14 10:40:12 -0500

    Q1-Q5

---
+++ scriptOne.js
@@ -0,0 +1,55 @@
+//Q1
+let name = "Scarlett";
+console.log(name);
+
+let age = "18";
+console.log(age);
+
+//Q2
+let pet = "dog";
+console.log(pet);
+
+let color = undefined;
+console.log(color);
+
+let number = 90;
+console.log(number);
+
+let question = true;
+console.log(question)
+
+//Q3
+let house;
+let rollNumber = "15";
+
+if (rollNumber > 10) {
+    house= "The RollNumber is greater than 10";
+    console.log(house)
+}
+//Q4
+let house2;
+ let isMember = true;
+
+ if (isMember = true) {
+    house2= "Member Discount applied";
+    console.log(house2)
+ }
+//Q5
+let house3;
+let startValue = 50;
+
+if (startValue > 0) {
+    house3= "Number is positive";
+    console.log(house3)
+}
+if (startValue = 0) {
+    house3= "Number is zero";
+    console.log(house3)
+}
+if (startValue < 0) {
+    house3= "Number is negative"
+    console.log(house3)
+}
+//Q6
+
+
