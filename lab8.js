19a43aa60bbdb488450087d4d30469f4403fba64
scarlettdaprile <skd53@njit.edu>
2024-11-19 10:02:46 -0500

    update

---
+++ lab8.js
@@ -0,0 +1,118 @@
+//Q1
+let name = "Scarlett";
+console.log(name);
+
+let age = "18";
+console.log(age);
+
+//Q2
+let pet = "dog";
+console.log(pet);
+
+let color = undefined;
+console.log(color);
+
+let number = 90;
+console.log(number);
+
+let question = true;
+console.log(question)
+
+//Q3
+let house;
+let rollNumber = "15";
+
+if (rollNumber > 10) {
+    house= "The RollNumber is greater than 10";
+    console.log(house)
+}
+//Q4
+let house2;
+ let isMember = true;
+
+ if (isMember = true) {
+    house2= "Member Discount applied";
+    console.log(house2)
+ }
+//Q5
+let house3;
+let startValue = 50;
+
+if (startValue > 0) {
+    house3= "Number is positive";
+    console.log(house3)
+}
+if (startValue = 0) {
+    house3= "Number is zero";
+    console.log(house3)
+}
+if (startValue < 0) {
+    house3= "Number is negative";
+    console.log(house3)
+}
+//Q6
+let a= "15"
+let b= "13"
+console.log(a == b)
+console.log(a === b)
+console.log(a != b)
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+//Q7
+let firstName = "Scarlett"
+console.log(firstName)
+let lastName = "Daprile"
+console.log(lastName)
+let myAge = 18
+console.log(myAge)
+console.log("Hello,", firstName, lastName, "you are", myAge, "years old")
+//Q8
+let num=100
+if (num>0 && num<100) {
+console.log ("The number is within range")
+}
+//Q9
+let hasCar = true
+
+let hasLicense = true
+
+let hasInsurance = true
+
+
+if (hasCar && hasLicense && hasInsurance) {
+   console.log("You can drive legally.");
+}
+else {
+   console.log("Check your driving eligibility.");
+}
+//Q10
+let num2=21
+if (num2 %2==0) {
+    console.log("The number is even.");
+}
+if (num2 %2==1){
+    console.log("The number is odd.")
+}
+
+//Q11
+let house4;
+let number2="85"
+
+if (number2 > 90) {
+    house = "Grade A";
+    console.log(house);
+}
+
+else if(number2>80){
+    house= "Grade B";
+    console.log(house);
+}
+
+else if(number2>70){
+    house="Grade C";
+    console.log(house);
+}
+
+
